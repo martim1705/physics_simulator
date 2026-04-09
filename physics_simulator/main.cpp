4768eee893149d034926fdc54695f6b145d1e553
martim1705 <up202304944@up.pt>
2026-04-09 01:28:58 +0100

    atrito do ar. perda

--- physics_simulator/main.cpp
+++ physics_simulator/main.cpp
@@ -4,8 +4,8 @@
 
 
 float g = 2354.4f; // pixels / segundo^2
-float perda_energia = 0.8; 
-
+float perda_energia = 0.7; 
+float atrito_ar = 200.f;
 
 // energia cinetica = 1/2 * m * v^2
 
@@ -26,8 +26,8 @@ int main()
     float x = 360.f; 
     float y = 240.f;
     float yf, xf;
-    float vy = 240; // pixel / segundo
-    float vx = 0;
+    float vy = 240.0f; // pixel / segundo
+    float vx = 0.0f;
      
     // posicionar na janela 
     shape.setPosition({ x, y });
@@ -56,8 +56,20 @@ int main()
                 window.close();
         }
 
+        // se a tecla da esquerda é selecionada, bola move-se para a esquerda
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
+            vx = -120.0f;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
+            vx = 120.0f;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) {
+            vy = -900.f;
+        }
+
+
         // velocidade 
-        vy +=  g * dt;
+        vy += g * dt;
 
         sf::Vector2f initial_position = shape.getPosition();
         yf = initial_position.y + vy * dt;
@@ -67,30 +79,39 @@ int main()
         sf::Vector2f final_position = shape.getPosition();
         
 
-        if (final_position.y > 430.f) { // colisões para o "chão
+        if (yf > 430.f) { // colisões para o "chão
             yf = 430.0f;
             vy = -(perda_energia * vy);
         }
-        else if (final_position.y <= 50.f) { // colisões para o "teto"
-            vy = -vy; 
+        else if (yf < 50.f) { // colisões para o "teto"
+            yf = 50.f;
+            vy = -perda_energia * vy;
         }
 
-        if (final_position.x <= 50.0f) {
-            vx = 120; 
+        if (xf < 50.0f) { // colisões à esquerda
+            xf = 50.0f; 
+            vx = -vx;
+
         }
-        else if (final_position.x >= 670.0f) {
-            vx = -120; 
+        else if (xf > 670.0f) { // colisões à direita
+            xf = 670.0f;
+            vx = -vx;
         }
 
-        // se a tecla da esquerda é selecionada, bola move-se para a esquerda
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
-            vx = -120;
+        
+        
+        if (vx > 0.f) {
+            vx -= atrito_ar * dt;
+            if (vx < 0.f) vx = 0.f;
         }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
-            vx = 120; 
+        else if (vx < 0) {
+            vx += atrito_ar * dt;
+            if (vx > 0.f) vx = 0.f;
         }
-
         shape.setPosition({ xf, yf });
+
+
+        std::cout << "Ball position: (" << xf << "," << yf << ")\n";
 
         // limpar e desenhar
         window.clear();
